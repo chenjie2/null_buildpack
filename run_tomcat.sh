c46340679c50cf518a2b84d49e83f64ee68cbd41
chenjie2 <54595811@163.com>
2014-04-28 11:22:04 +0800

    Update run_tomcat.sh

--- run_tomcat.sh
+++ run_tomcat.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 BIN_DIR=$(dirname $0)
-export JAVA_HOME=$BIN_DIR/../../openjdk
+#export JAVA_HOME=$BIN_DIR/../../openjdk
 export CATALINA_OPTS="-Dhttp.port=$PORT $CATALINA_OPTS"
 
 exec $BIN_DIR/catalina.sh run
